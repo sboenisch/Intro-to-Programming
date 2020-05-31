a3b193275e702507b50eead2c302a349661ba4a1
Sebastian Bönisch <49536022+sboenisch@users.noreply.github.com>
2020-05-31 12:50:14 +0200

    Updates

--- Python/turtle.py
+++ Python/turtle.py
@@ -249,3 +249,40 @@ if romeo_last_name == "montague":
         juliet.forward(1)
         juliet.right(1)
     juliet.hideturtle()
+
+# Modulo Operator
+def bead(tur):
+    tur.right(75)
+    for _ in range(12):
+        tur.forward(10)
+        tur.left(30)
+    tur.left(75)
+
+t = turtle.Turtle()
+t.speed(0)
+t.width(2)
+
+# Move to the left before starting.
+t.penup()
+t.back(200)
+t.pendown()
+
+# Draw ten beads.
+for n in range(10):
+    if n % 2 == 0:
+        t.color("blue")
+    else:
+        t.color("red")
+    bead(t)
+    t.forward(40)
+
+# Drawing stairs
+amy = turtle.Turtle()
+amy.color("red")
+
+for step in range(6):
+    amy.forward(50)
+    if(step%2==0):
+        amy.left(90)
+    else:
+        amy.right(90)
