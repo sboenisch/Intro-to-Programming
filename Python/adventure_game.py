e036b97e0ee605fc03c7866eda4d88e5efd8f2d2
Sebastian Bönisch <49536022+sboenisch@users.noreply.github.com>
2020-06-07 19:08:18 +0200

    Create adventure_game.py

---
+++ Python/adventure_game.py
@@ -0,0 +1,14 @@
+import time
+import random
+
+def time_sleep(text):
+    print(text)
+    time.sleep(2)
+
+#Intro of the adventure game
+time_sleep("""You find yourself standing in an open field, filled with
+grass and yellow wildflowers.""")
+time_sleep("""Rumor has it that a pirate is somewhere around here, and has
+been terrifying the nearby village.""")
+time_sleep("""In front of you is a house. To your right is a dark cave.
+In your hand you hold your trusty dagger.""")
