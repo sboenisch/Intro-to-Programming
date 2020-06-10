32da7264ffe38d8c03691c9d82b73b753b56cd14
Sebastian Bönisch <49536022+sboenisch@users.noreply.github.com>
2020-06-10 19:33:17 +0200

    updates

--- Python/adventure_game2.py
+++ Python/adventure_game2.py
@@ -1,61 +1,83 @@
 import time
 import random
 
-#Grabbing objects
-points = random.randint(2,50)
-assists = random.randint(0,15)
-steals = random.randint(0,10)
+# Random game stats for the player
+# points = random.randint(2, 50)
+# assists = random.randint(0, 15)
+# steals = random.randint(0, 10)
 
-choices = ("You can only chose between A, B or C.")
+# Warning for invalid enters
+choices = ("You can only choose between A, B or C.")
 
 
-#Delays the print function
+# Delays the print function
 def printSleep(text):
-    print(text +"\n")
-    time.sleep(2)
+    print(text + "\n")
+    time.sleep(1)
 
 
-#Introduction of the adventure game
+def stats():
+    print("Points: " + str(points))
+    print("\nAssists: " + str(assists))
+    print("\nSteals: " + str(steals))
+    replay()
+
+
+# Introduction of the adventure game Miami Heat vs. Chicago Bulls
 def start():
+    global points, assists, steals
+    points = random.randint(2, 50)
+    assists = random.randint(0, 15)
+    steals = random.randint(0, 10)
     printSleep("Today is your great game against Chicago Bulls with their")
     printSleep("superstar Michael Jordan. Your long term rival.")
     printSleep("You prepared weeks for this insane game.")
     printSleep("The game is about to end. The current scores are 88:88.")
     printSleep("10 seconds are left.")
     printSleep("You have the ball. What will you do?")
-    #Choices
-    printSleep("A. Pass the ball to your teammate.")
+    printSleep("A. Pass the ball to your teammate.")  # Choices
     printSleep("B. Triple the ball towards 3-point line.")
     printSleep("C. Stand still.")
     answer = input(">>> ").lower()
-    if "a" in answer:
+    if answer == "a":
         teammate()
-    elif "b" in answer:
+    elif answer == "b":
         threePointLine()
-    elif "c" in answer:
+    elif answer == "c":
         scottiePippen()
     else:
         printSleep(choices)
         start()
 
 
-#Option throwing a rock to the orc
+# Option throwing a rock to the orc
 def teammate():
     printSleep("You pass the ball to your teammate Dwayne Wade.")
-    printSleep("running towards you again. Will you:")
-    printSleep("A. Run\nB. Throw another rock\nC. Run towards a nearby cave\n")
+    printSleep("He is about to do his signature move aka the Flash.")
+    printSleep("What will you do?")
+    printSleep("""A: Nothing\nB: Going to the basket \n C: Set a block
+    for your teammate.""")
     answer = input(">>> ").lower()
-    if "a" in answer:
-        # option_run()
-    elif "b" in answer:
-        # printSleep("You decided to throw another rock, as if the first")
-        # printSleep("rock thrown did much damage. The rock flew well over the ")
-        # printSleep("orcs head. You missed. \n\nYou died!")
-    elif "c" in answer:
-        # option_cave()
+    if answer == "a":
+        printSleep("""Dwayne Wade looses the ball and the Chicago Bulls
+        scores to the win.""")
+        printSleep("Chicago wins the game by 90:88.")
+        stats()
+    elif answer == "b":
+        printSleep("Your teammate shoots! But he misses the basket.")
+        printSleep("""The ball is still in the air. Thankfully your position
+        is good.""")
+        printSleep("""You rebound the ball and put it in!!! Miami Heat wins
+        90:88""")
+        stats()
+    elif answer == "c":
+        printSleep("""By setting up the block your teammate had the chance to
+        go to the basket.""")
+        printSleep("He scores!!! Miami Heat wins 90:88.")
+        stats()
     else:
         print(choices)
-        # option_rock()
+        teammate()
 
 
 def threePointLine():
@@ -65,22 +87,32 @@ def threePointLine():
         printSleep("What do you do next?")
         printSleep("A. Cross over\nB. Throw 3 point\nC. Run towards basket")
         answer = input(">>> ").lower()
-        if "a" in answer:
-            # printSleep("Really? You're going to hide in the dark? I think ")
-            # printSleep("orcs can see very well in the dark, right? Not sure, but ")
-            # printSleep("I'm going with YES, so...\n\nYou died!")
+        if answer == "a":
+            printSleep("""You cross the ball over but Michael Jordan steals you
+            the ball.""")
+            printSleep("""2 seconds later M.J. scores and lead the Chicago
+            Bulls to the win.""")
+            stats()
             break
-        elif "b" in answer:
-            # printSleep("You laid in wait. The shimmering sword attracted ")
-            # printSleep("the orc, which thought you were no match. As he walked ")
-            # printSleep("closer and closer, your heart beat rapidly. As the orc ")
-            # printSleep("reached out to grab the sword, you pierced the blade into ")
-            # printSleep("its chest. \n\nYou survived!")
+        elif answer == "b":
+            printSleep("""Only 3 seconds left. You jump into the air and shoot
+            the ball.""")
+            printSleep("""The ball touches the ring and bounce against the
+            basket wall.""")
+            printSleep("""A moment of silence. But suddenly the ball falls
+            into the basket.""")
+            printSleep("Miami Heat wins 91:88.")
+            stats()
             break
-        elif "c" in answer:
-            # printSleep("As the orc enters the dark cave, you sliently ")
-            # printSleep("sneak out. You're several feet away, but the orc turns ")
-            # printSleep("around and sees you running.")
+        elif answer == "c":
+            printSleep("""You pass the first enemy player. Then the second
+            enemy player.""")
+            printSleep("""""You lift off for the points but suddenly a huge
+            figure is""")
+            printSleep("in front of you and blocks you away.")
+            printSleep("""A quick pass and Chicago Bulls scores and wins
+            the game!""""")
+            stats()
             break
 
 
@@ -89,14 +121,14 @@ def scottiePippen():
     printSleep("You will:")
     printSleep("A. Do nothing.\nB. Foul Pippen\nC. Try to steal the ball.")
     answer = input(">>> ").lower()
-    if "a" in answer:
+    if answer == "a":
         printSleep("Pippen scores. Chicago Bulls won 90:88. You lost.")
         stats()
-    elif "b" in answer:
+    elif answer == "b":
         printSleep("You fouled Pippen but too late. Pippen scores")
         printSleep("and scored the free throw, too. You lost.")
         stats()
-    elif "c" in answer:
+    elif answer == "c":
         stealBall()
     else:
         printSleep(choices)
@@ -108,11 +140,10 @@ def stealBall():
     printSleep("7...6...5...You pass the ball to your teammate.")
     printSleep("Will you run towards the basket [Y/N]?")
     answer = input(">>> ").lower()
-    if "y" in answer:
-        assit += 1
+    if answer == "y":
         print("Your teammates throw and hits!!!! Miami Heat wins 91:88")
         stats()
-    elif "n" in answer:
+    elif answer == "n":
         printSleep("You did nothing. Your teammates misses the throw. ")
         printSleep("Game ends. It is a tie 88:88")
         stats()
@@ -121,19 +152,15 @@ def stealBall():
         stealBall()
 
 
-def stats():
-    print("Points: " + points + "\nAssists: " + assists + "\nSteals: " + steals)
-
-
 def replay():
-    gamechoice = input("You wanna play again [Y/N]?"").lower()
+    gamechoice = input("You wanna play again [Y/N]?").lower()
     while True:
-    if "y" in gamechoice:
-        start()
-        break
-    elif "n" in gamechoice:
-        printSleep("Thank you \f\o\r playing the game!")
-        break
+        if gamechoice == "y":
+            start()
+            break
+        elif gamechoice == "n":
+            printSleep("Thank you for playing the game!")
+            break
 
 
 start()
